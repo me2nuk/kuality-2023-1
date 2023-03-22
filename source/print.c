239b6cd07236239be77b8767a1da920bb8073548
me2nuk <57348147+me2nuk@users.noreply.github.com>
2023-03-22 20:54:33 +0900

    Update

---
+++ source/print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("안녕하세요우 format string에 대해 알아보겠쓰무니다\n");
+    printf("먼저 format string의 syntax 으로는 %%[parameter][flags][width][.precision][length]type 으로 이루어져 있습니다.\n");
+    printf("그 외에도 접할 일이 거의 없는 format string 중에 %%2$d 와 같은 형식도 존재하무니다");
+
+    return 0;
+}
---
+++ source/scanf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char x[6];
+
+    scanf("%s", &x);
+    printf("Hello %s", x);
+
+    return 0;
+}
